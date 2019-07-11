4df531a6ef4c297f8ea73e8f9dadcedb901d99ce
hyangjee <giwl1@naver.com>
2019-07-12 08:54:47 +0900

    gugudan

--- gugu.c
+++ gugu.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-	int i,j;
-	for(i=1,i<;i++)
+	for(int i=1;i<10;i++)
 	{
 		printf("%d dan \n",i);
-		for(j=1;j<=5000;i++)
-			printf("%d x %d = %d \n",i,j,i*j);
+		for(int j=1;j<=10;i++)
+		{	printf("%d x %d = %d \n",i,j,(i*j));}
 		printf("\n");
+		
 	}
 	return 0;
 }
